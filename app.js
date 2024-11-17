8b13512f9267bd2c316d4265dbb41a7579a73122
Elvin251 <emmmdov485@gmail.com>
2024-11-17 18:49:55 +0400

    d

---
+++ app.js
@@ -0,0 +1,7 @@
+ let number=prompt("Bir ədəd daxil edin:");
+
+if (number%2==0) {
+    document.write ("daxil etdiyin eded cut ededdir");
+} else {
+   document.write ( "daxil etdiyin eded tek ededdir");
+}
